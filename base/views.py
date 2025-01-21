5a4ba62538b66326c3c9786f74bd314daa5e950a
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-01-21 15:07:46 +0200

    Modify getUsers

--- base/views.py
+++ base/views.py
@@ -1,27 +1,32 @@
-from account.models import *
 from base.models import *
+from account.models import *
+from base.serializers import *
 from django.http import Http404
 from account.serializers import *
-from base.serializers import *
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
-# User Views
 class getUsers(APIView):
     """
     Retrieve a list of all users.
     """
+    permission_classes = [IsAuthenticated]
+
     def get(self, request, format=None):
         try:
             users = User.objects.all()
             serializer = UserSerializer(users, many=True)
             message = {"detail": "Successfully retrieved all users."}
-            return Response({"data": serializer.data, "message": message}, status=status.HTTP_200_OK)
+            return Response(
+                {"data": serializer.data, "message": message},
+                status=status.HTTP_200_OK
+            )
         except Exception as e:
             message = {
                 "detail": "An error occurred while retrieving users.",
-                "error": str(e)
+                "error": str(e),
             }
             return Response({"message": message}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
