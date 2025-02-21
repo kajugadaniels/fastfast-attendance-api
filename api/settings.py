6f92dd35a838fd9b72239ffa9a56606d83995f22
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-02-21 16:40:35 +0200

    New changes

--- api/settings.py
+++ api/settings.py
@@ -98,12 +98,8 @@ if str(os.getenv("NODE_ENV"))=="production":
 else:
     DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv("MYSQL_DB"),
-        'USER': os.getenv("MYSQL_USER"),
-        'PASSWORD': os.getenv("MYSQL_PASSWORD"),
-        'HOST': os.getenv("MYSQL_HOST"),
-        'PORT': os.getenv("MYSQL_PORT", "3306"),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
